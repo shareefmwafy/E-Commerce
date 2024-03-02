8e4af76b55a1cdf2d710cebe81eb71e82797ca09
shareef <s12029160@stu.najah.edu>
2024-03-02 02:46:15 +0200

    Hero Section

--- src/Home-Page/Hero/Hero.jsx
+++ src/Home-Page/Hero/Hero.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
-
+import Style from './Hero.module.css'
+import image1 from '../../assets/1.jpg'
+import image2 from '../../assets/2.png'
+import image3 from '../../assets/3.jpg'
+import image4 from '../../assets/4.jpg'
 function Hero() {
   return (
-    <div>Hero</div>
+    
+   <>
+    <div className={Style.container}>
+      <div className={Style.row}>
+        <div className={Style.shop}><img src={image1} alt="" /></div>
+        <div className={Style.images}>
+          <div className={Style.up}><img src={image2} alt="" /></div>
+
+          <div className={Style.down}>
+            <div className={Style.downLeft}><img src={image3} alt="" /></div>
+            <div className={Style.downRight}><img src={image4} alt="" /></div>
+          </div>
+        </div>
+      </div>
+    </div>
+   </>
+    
   )
 }
 
