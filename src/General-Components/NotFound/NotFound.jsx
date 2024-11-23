c1693581bacc08e3a895debbb4a2a7edc4e5c8fa
Shareef Mwafy <sh.mwafy1@gmail.com>
2024-11-23 16:56:14 +0200

    Create Not Found Page

--- src/General-Components/NotFound/NotFound.jsx
+++ src/General-Components/NotFound/NotFound.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import './NotFound.css';
 
 function NotFound() {
   return (
-    <div>NotFound</div>
-  )
+    <div className="not-found-container">
+      <h1 className="not-found-title">404</h1>
+      <p className="not-found-text">Oops! The page you are looking for does not exist.</p>
+      <a href="/" className="back-home-link">Go Back Home</a>
+    </div>
+  );
 }
 
-export default NotFound
+export default NotFound;
